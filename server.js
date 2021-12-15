117f771805e8ad219c11086e0492c8d9aa2d47fb
Mikko <mikko.vallaskivi@digia.com>
2021-12-16 00:54:26 +0200

    Dice Game: added socket io support to server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+// Imports
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const sockets = require('socket.io')
+
+// Server Creation
 const app = express()
-//env.PORT specified here for future deployment
+// Direct access to http package method required in order to use socketio
+const server = http.createServer(app)
+// Create socket io server
+const io = sockets(server)
+
+// Socket io connection
+io.on('connection', socket => {
+    console.log('New connection established!')
+})
+// env.PORT specified here for future deployment
 const PORT = 3000 || process.env.PORT
 // Set static folder for UI
 app.use(express.static(path.join(__dirname, 'UI')))
 
 // run server
-app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+server.listen(PORT, () => console.log(`Server running at ${PORT}`))
