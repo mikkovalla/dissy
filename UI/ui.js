07895f8ebd820aff30c36ecb39141963af98b645
Mikko <mikko.vallaskivi@digia.com>
2021-12-16 01:43:41 +0200

    Dice Game: Roll button returns random die number

--- UI/ui.js
+++ UI/ui.js
@@ -1,7 +1,19 @@
 // Access imported socket io
 const socket = io();
 
-// Emit welcome message
+// Emit message
 socket.on('message', message => {
     console.log(message)
 })
+
+// Access dice roll button
+const rollButton = document.getElementById('game-form')
+rollButton.addEventListener('submit', e => {
+    e.preventDefault()
+    const dieValue = rolledDieNumber()
+    console.log(dieValue)
+})
+
+function rolledDieNumber() {
+    return Math.floor(Math.random() * 6) + 1
+}
