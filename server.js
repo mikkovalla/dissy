ecb3d5e8143a9430a824789565c0ba986c83b2e2
Mikko <mikko.vallaskivi@digia.com>
2021-12-16 00:37:31 +0200

    Dice Game: UI renders

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 //env.PORT specified here for future deployment
 const PORT = 3000 || process.env.PORT
+// Set static folder for UI
+app.use(express.static(path.join(__dirname, 'UI')))
 
 // run server
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
