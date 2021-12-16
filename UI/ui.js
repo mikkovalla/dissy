88a9abbdb8aeec90714900b04254321a5630ff00
Mikko <mikko.vallaskivi@digia.com>
2021-12-16 02:02:49 +0200

    Dice Game: die roll renders on UI

--- UI/ui.js
+++ UI/ui.js
@@ -3,7 +3,8 @@ const socket = io();
 
 // Emit message
 socket.on('message', message => {
-    console.log(message)
+    //console.log(message)
+    renderMessage(message)
 })
 
 // Access dice roll button
@@ -19,4 +20,18 @@ rollButton.addEventListener('submit', e => {
 // function return random number between 1 - 6
 function rolledDieNumber() {
     return Math.floor(Math.random() * 6) + 1
+}
+
+// Function to render messages from server to UI. includes rolled die scores
+function renderMessage(dieValue) {
+    const div = document.createElement('div')
+    div.classList.add('result')
+    // Never do this in prod, leaves a path for a XSS attack. Done here for speed and simplicity
+    div.innerHTML = 
+        `<p class="meta">Mixu <span>9:15pm</span></p>
+            <p class="score">
+                ${dieValue}
+        </p>`
+    // append results to UI
+    document.querySelector('.game-rolls').appendChild(div)
 }
